e91ff18a5bd4cb9c201654068dc0b05a4e0e4fe5
Yui Yukihira <yuiyukihira@pme.me>
2019-08-29 01:59:59 +0100

    Convert example auth service to use the new class

--- src/roamrs/auth.py
+++ src/roamrs/auth.py
@@ -1,24 +1,25 @@
-from .services import Service
+from .services import AuthService
 from aiohttp import ClientSession
+from typing import Any, Dict
 
-class TokenValidator(Service):
+class TokenValidator(AuthService):
     __slots__ = ('url')
-    def __init__(self, url, *args):
+    def __init__(self, url, *args, **kwargs):
         self.url = url.rstrip('/')
-        self.__session = ClientSession()
+        self.__session = ClientSession(*args, **kwargs)
 
-    async def __call__(self, token):
-        async with self.__session.get(self.url+'/verify', headers={'Authorization': token}) as resp:
+    async def __call__(self, auth_str: str) -> bool:
+        async with self.__session.get(self.url+'/verify', headers={'Authorization': auth_str}) as resp:
             if resp.status == 200:
                 return True
             if resp.status == 401:
                 return False
             return False
 
-    async def get_user(self, token):
-        async with self.__session.get(self.url+'/get_user', headers={'Authorization': token}) as resp:
+    async def get_user(self, auth_str: str) -> Dict[str, Any]:
+        async with self.__session.get(self.url+'/get_user', headers={'Authorization': auth_str}) as resp:
             if resp.status == 200:
                 return await resp.json()
             if resp.status == 401:
-                return {}
-            return {}
+                return None
+            return None
